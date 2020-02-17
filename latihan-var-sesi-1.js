647f90f71193f534272c84201d2695e87892b3b9
Dika Rachmansyah konsultan <dika.rachmansyah.konsultan@work.bri.co.id>
2020-02-17 21:32:56 +0700

    dika

---
+++ latihan-var-sesi-1.js
@@ -0,0 +1,19 @@
+let message = "Perkenalkan saya"
+let myName
+let whyChooseHacktiv8 = "Karena "
+let completeMessage
+
+// Your Code Here
+
+// myName = " Ananda Ning Saputra, "
+// whyChooseHacktiv8 = whyChooseHacktiv8 + "saya ingin mengupgrade skill di ReactJS"
+// completeMessage = message + myName + whyChooseHacktiv8
+
+// Second Code
+myName = " Muvid "
+completeMessage = message+myName+whyChooseHacktiv8+"Saya suka coding"
+
+
+console.log(completeMessage)
+// Perkenalkan saya Ananda Ning Saputra, Karena saya ingin mengupgrade skill di ReactJS 
+// Perkenalkan saya {nama Kalian} Karena {alasan Kalian}
